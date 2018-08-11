1d64ba2d089bce3c6abf5f669cbc9f66fb338645
ming <jg521521@qq.com>
2018-08-11 12:44:54 +0800

    commit php

---
+++ php/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>主页</title>
+  <meta charset="utf-8">
+</head>
+<body>
+<?php 
+  $color= "red";
+  echo "fuck  ".$color."  zhangming"
+  
+?>
+</body>  
+</html>
